6ce7cebeb1865477e3d81bd52484415aff90676f
Yuki <wyy2075223281@126.com>
2025-03-11 21:55:15 +0800

    code format

--- bin_tree.cpp
+++ bin_tree.cpp
@@ -1,88 +1,110 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
 int depth = 0;
 
-//define a struct for binTree
-struct node{
+// define a struct for binTree
+struct node
+{
     int value;
-    node* left;
-    node* right;
-    node(int value) : value(value), left(nullptr), right(nullptr) {
+    node *left;
+    node *right;
+    node(int value) : value(value), left(nullptr), right(nullptr)
+    {
         // this->value = value;
     }
 
-    void print() {
+    void print()
+    {
         cout << this->value << endl;
     }
 };
 
-node* addNode(node* root, int v) {
-    if(root==nullptr){
+node *addNode(node *root, int v)
+{
+    if (root == nullptr)
+    {
         return new node(v);
     }
-    if(root->value > v){
-        root->left=addNode(root->left,v);
-    }else{
-        root->right=addNode(root->right,v); 
+    if (root->value > v)
+    {
+        root->left = addNode(root->left, v);
+    }
+    else
+    {
+        root->right = addNode(root->right, v);
     }
     return root;
 }
 
-void midprint(node* root){
-    if(root==nullptr){
+void midprint(node *root)
+{
+    if (root == nullptr)
+    {
         return;
     }
     midprint(root->left);
-    cout<<root->value<<" ";
+    cout << root->value << " ";
     midprint(root->right);
-
 }
-int Depth(node* root) {
-    if (root == nullptr) {
+int Depth(node *root)
+{
+    if (root == nullptr)
+    {
         return 0;
     }
     int leftDepth = Depth(root->left);
     int rightDepth = Depth(root->right);
-    
+
     return max(leftDepth, rightDepth) + 1;
 }
 
-bool search(node* current, int val){
-    if(current == nullptr){
+bool search(node *current, int val)
+{
+    if (current == nullptr)
+    {
         return 0;
     }
-    if(val < current->value){
+    if (val < current->value)
+    {
         return search(current->left, val);
-    }else if(val == current->value){
+    }
+    else if (val == current->value)
+    {
         return 1;
-    }else{
-        return search(current->right, val); 
+    }
+    else
+    {
+        return search(current->right, val);
     }
 }
 
 int main()
 {
-   node* root=nullptr;
-   node* test = new node(10);
-   test->print();
+    node *root = nullptr;
+    node *test = new node(10);
+    test->print();
 
-   while(1){
-    int a;
-    cin>>a;
-    // cout<<a<<endl;
-    if (a < 0) {
-        break;
-    } else{
-       root = addNode(root,a);
+    while (1)
+    {
+        int a;
+        cin >> a;
+        // cout<<a<<endl;
+        if (a < 0)
+        {
+            break;
+        }
+        else
+        {
+            root = addNode(root, a);
+        }
     }
-   }
-//    cout<<"test"<<endl;
-   midprint(root);
-   int value;
-   cout<<"input a value to search:";
-   cin>>value;
-   cout<<search(root, value)<<endl;
+    //    cout<<"test"<<endl;
+    midprint(root);
+    int value;
+    cout << "input a value to search:";
+    cin >> value;
+    cout << search(root, value) << endl;
 
     return 0;
 }
--- profit_calculatar.cpp
+++ profit_calculatar.cpp
@@ -1,47 +1,48 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int money,profit;
-    int start,end;
-    cout<<"请输入你的理财本金和收益（不包含本金），以回车结束"<<endl;
-    cin>>money>>profit;
-    cout<<"请输入你的购买日期和取款日期，如「20240110」，以回车结束"<<endl;
-    cin>>start>>end;
-   //日期处理
-   int y0,m0,d0,y1,m1,d1;
-   y0=start/10000;
-   start=start-(y0*10000);
-   y1=end/10000;
-   end=end-(y1*10000);
-   m0=start/100;
-   start=start-(m0*100);
-   m1=end/100;
-   end=end-(m1*100);
-   d0=start;
-   d1=end;
+    int money, profit;
+    int start, end;
+    cout << "请输入你的理财本金和收益（不包含本金），以回车结束" << endl;
+    cin >> money >> profit;
+    cout << "请输入你的购买日期和取款日期，如「20240110」，以回车结束" << endl;
+    cin >> start >> end;
+    // 日期处理
+    int y0, m0, d0, y1, m1, d1;
+    y0 = start / 10000;
+    start = start - (y0 * 10000);
+    y1 = end / 10000;
+    end = end - (y1 * 10000);
+    m0 = start / 100;
+    start = start - (m0 * 100);
+    m1 = end / 100;
+    end = end - (m1 * 100);
+    d0 = start;
+    d1 = end;
     int days;
-    //将日期差转化为天数
-    int start_days=360*y0+(m0-1)*30+d0;
-    int end_days=360*y1+(m1-1)*30+d1;
-    if(start_days>=end_days){
-        cout<<"invalid input"<<endl;
+    // 将日期差转化为天数
+    int start_days = 360 * y0 + (m0 - 1) * 30 + d0;
+    int end_days = 360 * y1 + (m1 - 1) * 30 + d1;
+    if (start_days >= end_days)
+    {
+        cout << "invalid input" << endl;
         return 0;
     }
-    days = end_days-start_days;
+    days = end_days - start_days;
 
-    float profit_per_day,cache0,cache1;
-    cache0=profit;
-    cache1=days;
-    profit_per_day = cache0/cache1;
-    //日均收益
-    float daily_profit_rate,cache3;
-    cache3=money;
-    daily_profit_rate = profit_per_day/cache3;
+    float profit_per_day, cache0, cache1;
+    cache0 = profit;
+    cache1 = days;
+    profit_per_day = cache0 / cache1;
+    // 日均收益
+    float daily_profit_rate, cache3;
+    cache3 = money;
+    daily_profit_rate = profit_per_day / cache3;
     daily_profit_rate = daily_profit_rate * 100;
     // cout<<"da"<<daily_profit_rate;
-    float year_prof=daily_profit_rate*360;
-    cout<<"年化利率"<<year_prof<<"%"<<endl;
+    float year_prof = daily_profit_rate * 360;
+    cout << "年化利率" << year_prof << "%" << endl;
     return 0;
 }
